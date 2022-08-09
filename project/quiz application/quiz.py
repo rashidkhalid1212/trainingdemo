10b9b852748ed9769f97387efa0d18c78d617715
khalid rashid <rashidkhalid9114@gmail.com>
2022-08-09 11:46:46 +0530

    project updated

--- project/quiz application/quiz.py
+++ project/quiz application/quiz.py
@@ -42,9 +42,17 @@ def admin_w():
 
     window_admin.title("Admin")
     window_admin.geometry("900x500")
+    window_admin.config(bg = 'light blue')
 
     def add_q():
-        no_ques = IntVar()
+        try:
+            note_2.destroy()
+            txt.destroy()
+            save_bt.destroy()
+
+        except:
+            NameError()
+
 
         def add_question(n):
             ques_1 = StringVar()
@@ -88,13 +96,14 @@ def admin_w():
                 # done_l.place(x = 500, y = 200)
 
 
-
+            global ques1
             ques1 = Label(
                 window_admin,
                 text=f'Question {n}:'
             )
             ques1.place(x = 70, y = 130)
 
+            global ques1_ent
             ques1_ent = Entry(
                 window_admin, 
                 textvariable=ques_1,
@@ -102,11 +111,28 @@ def admin_w():
             )
             ques1_ent.place(x = 70, y = 150)
 
-            op1 = Label(window_admin, text='Option 1:  a)').place(x = 80, y = 190)
-            op2 = Label(window_admin, text='Option 2:  b)').place(x = 80, y = 220)
-            op3 = Label(window_admin, text='Option 3:  c)').place(x = 80, y = 250)
-            op4 = Label(window_admin, text='Option 4:  d)').place(x = 80, y = 280)
-            ans = Label(window_admin, text='Answer: ').place(x = 80, y = 320)
+            global op1
+            global op2
+            global op3
+            global op4
+            global ans
+
+            op1 = Label(window_admin, text='Option 1:  a)')
+            op1.place(x = 80, y = 190)
+            op2 = Label(window_admin, text='Option 2:  b)')
+            op2.place(x = 80, y = 220)
+            op3 = Label(window_admin, text='Option 3:  c)')
+            op3.place(x = 80, y = 250)
+            op4 = Label(window_admin, text='Option 4:  d)')
+            op4.place(x = 80, y = 280)
+            ans = Label(window_admin, text='Answer: ')
+            ans.place(x = 80, y = 320)
+
+            global op1_ent
+            global op2_ent
+            global op3_ent
+            global op4_ent
+            global ans_ent
 
             op1_ent = Entry(window_admin, textvariable= op_1)
             op1_ent.place(x = 220, y = 190)
@@ -119,20 +145,22 @@ def admin_w():
             ans_ent = Entry(window_admin, textvariable=ans_w)
             ans_ent.place(x = 220, y = 320)
 
-            
+            global nxt
             nxt = Button(window_admin, text='Next', width=19, command=lambda:next_q(ques1_ent, op1_ent, op2_ent, op3_ent, op4_ent, ans_ent))
             nxt.place(x = 500, y = 400)
 
+            global done
             done = Button(window_admin, text="Done", width=19, command=lambda:done_q(ques1_ent, op1_ent, op2_ent, op3_ent, op4_ent, ans_ent))
             done.place(x = 660, y = 400)
  
         
-        n = Label(
+        global note
+        note = Label(
             window_admin,
             text="*Note:- Just enter the question, its options and answer and, for the next questions click next and add the questions\n When all the questions are added click done",
             font=('Comic Sans MS', '10')
         )
-        n.place(x = 70, y = 70)
+        note.place(x = 70, y = 70)
 
         add_question(1)
 
@@ -144,11 +172,65 @@ def admin_w():
         fl.close()
 
 
-
     def edit_q():
-        pass
+        try:
+            note.destroy()
+            ques1.destroy()
+            ques1_ent.destroy()
+            op1.destroy()
+            op2.destroy()
+            op3.destroy()
+            op4.destroy()
+            ans.destroy()
+            op1_ent.destroy()
+            op2_ent.destroy()
+            op3_ent.destroy()
+            op4_ent.destroy()
+            ans_ent.destroy()
+            nxt.destroy()
+            done.destroy()
 
+        except:
+            NameError()
 
+        global note_2
+        note_2 = Label(
+            window_admin,
+            text='''IMPORTANT INSTRUCTIONS:-
+                  1. Each line contains a question with there options and answer
+                  2. The questions are in the format:-
+                     question |-| option1 |-| option2 |-| option3 |-| option4 |-| answer
+                  3. To edit a question, just change the question, options or the answer and then save it in this format''',
+            justify= 'left',
+            relief='raised',
+            pady = 10,
+            padx = 8
+        )
+        note_2.place(x = 90, y =280) 
+
+        def save_text():
+            text_file_edit = open("questionlist.txt", 'w')
+            text_file_edit.write(txt.get(1.0, END))
+
+            messagebox.showinfo("Message", "Changes saved")
+
+        global txt
+        txt = Text(
+            window_admin,
+            height=12,
+            width=90
+            )
+        txt.place(x = 90, y=60)
+
+        text_file_edit = open("questionlist.txt", 'r')
+        t = text_file_edit.read()
+
+        txt.insert(END, t)
+        text_file_edit.close()
+
+        global save_bt
+        save_bt = Button(window_admin, text='Save', width=19, command=save_text)
+        save_bt.place(x = 660, y = 400)
 
 
     bt1 = Button(window_admin, text="Add", width= 8, command=add_q)
@@ -161,11 +243,14 @@ def admin_w():
 
     window_admin.mainloop()
 
+
+
 def student_w():
     window_student = tkinter.Tk()
 
     window_student.title("Student")
     window_student.geometry("1200x700")
+
 
 
     window_student.mainloop()
@@ -176,7 +261,7 @@ def student_w():
 root = tkinter.Tk()
 
 root.title("Quiz")
-root.geometry("500x550")
+root.geometry("480x530")
 
 img_icon = PhotoImage(file = "quiz_icon.png")
 icon = Label(
@@ -217,7 +302,7 @@ bt_student.pack()
 text1 = Label(
     root, 
     text = "If you want to edit questions click \"Admin\" \n if you want to solve the questions click \'student\'",
-    font = ("Chiller", 12, 'bold'),
+    font = ("Chiller", 14, 'bold'),
     pady = 40
 )
 text1.pack()
